a849fa3d21464cfccb57f59fe69ee527e36a039a
sanzous <canastraleonard123@gmail.com>
2022-08-20 16:20:14 -0500

    Finished Adding MVC and Database to Project

--- backend/controllers/characterController.js
+++ backend/controllers/characterController.js
@@ -7,7 +7,8 @@ const Character = require('../models/characterModel')
 // GET /api/characters
 // Private
 const getCharacters = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get all kamehameha' })
+    const characters = await Character.find()
+    res.status(200).json(characters)
 })
 //set character
 // POST /api/characters
@@ -17,19 +18,42 @@ const setCharacter = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add text')
     }
-    res.status(200).json({ message: 'set kamehameha' })
+
+    const character = await Character.create({
+        text: req.body.text
+    })
+    res.status(200).json(character)
 })
 //update character
 // put /api/characters/:id
 // Private
 const updateCharacter = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'new all kamehameha' })
+    const character = await Character.findById(req.params.id)
+
+    if (!character) {
+        res.status(400)
+        throw new Error('Character not found')
+    }
+
+    const updatedCharacter = await Character.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedCharacter)
 })
 //delete character
 // delete /api/characters/:id
 // Private
 const deleteCharacter = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'remove all kamehameha' })
+    const character = await Character.findById(req.params.id)
+
+    if (!character) {
+        res.status(400)
+        throw new Error('Character not found')
+    }
+
+    await character.remove()
+
+    res.status(200).json({ id: req.params.id })
+
 })
 
 module.exports = {
