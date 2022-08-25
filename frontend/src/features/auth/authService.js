771e741969c26d905b6058d0e622ed74aeaddce5
sanzous <canastraleonard123@gmail.com>
2022-08-25 08:45:33 -0500

    Fixed frontend commit

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -12,10 +12,29 @@ const register = async (userData) => {
 
     return response.data
 }
+//Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+//Logout
+const logout = () => {
+    localStorage.removeItem('user')
+}
+
+
 
 
 const authService = {
-    register
+    register,
+    logout,
+    login
 }
 
 export default authService
